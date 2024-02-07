9b6cf9b8761ce6ea8db33fc8259cc3087a3f7513
Abdulrahman Rashad <128964610+AbdulrahmanRashad@users.noreply.github.com>
2024-02-07 19:55:21 +0200

    Set Default Color to Black

--- PaintProject.java
+++ PaintProject.java
@@ -1,25 +1,24 @@
 package PaintProject;
 
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.*;
 
 public class PaintProject extends JFrame {
 
@@ -32,10 +31,8 @@ public class PaintProject extends JFrame {
     public static void main(String[] args) {
         //PaintProject frame = new PaintProject();
         JFrame frame = new JFrame();
-        DrawPanel drawPanel = null;
-        ButtonPanel buttonPanel = new ButtonPanel(drawPanel); // Pass DrawPanel object to ButtonPanel constructor
-
-        drawPanel = new DrawPanel();
+        DrawPanel drawPanel = new DrawPanel();
+        ButtonPanel buttonPanel = new ButtonPanel(drawPanel);
         frame.setSize(1500, 1000);
 
         //frame.setContentPane(drawPanel);
@@ -54,15 +51,16 @@ public class PaintProject extends JFrame {
 
 class ButtonPanel extends JPanel {
 
-    private final DrawPanel drawPanel; // Declare drawPanel as an instance variable
-
     JCheckBox checkFilled;
     JButton btnRed, btnGreen, btnBlue, btnRectangle,
             btnOval, btnLine, btnPencil, btnEraser,
             btnClear, btnUndo, btnSave, btnOpen;
 
     public ButtonPanel(final DrawPanel drawPanel) {
-        this.drawPanel = drawPanel; // Assign drawPanel to the instance variable
+
+        /* Just default State*/
+        drawPanel.setCurrentShape("Pencil");
+        drawPanel.setCurrentColor(Color.BLACK);
 
         checkFilled = new JCheckBox("Fill");
         checkFilled.setPreferredSize(new Dimension(50, 40));
@@ -73,6 +71,7 @@ class ButtonPanel extends JPanel {
                     drawPanel.setFilled(true);
                 } else {
                     drawPanel.setFilled(false);
+
                 }
             }
         });
@@ -98,11 +97,56 @@ class ButtonPanel extends JPanel {
             }
         });
 
-        btnRectangle = createShapeButton("Rectangle");
-        btnOval = createShapeButton("Oval");
-        btnLine = createShapeButton("Line");
-        btnPencil = createShapeButton("Free Hand");
-        btnEraser = createShapeButton("Eraser");
+        btnRectangle = createIconButton(Color.WHITE, "Rectangle.png", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawPanel.setCurrentShape("Rectangle");
+                if (drawPanel.getCurrentColor() == Color.WHITE) {
+                    drawPanel.setCurrentColor(Color.BLACK);
+                }
+            }
+        });
+
+        btnOval = createIconButton(Color.WHITE, "Oval.png", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawPanel.setCurrentShape("Oval");
+                if (drawPanel.getCurrentColor() == Color.WHITE) {
+                    drawPanel.setCurrentColor(Color.BLACK);
+                }
+            }
+        });
+
+        btnLine = createIconButton(Color.WHITE, "Line.jpeg", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawPanel.setCurrentShape("Line");
+                if (drawPanel.getCurrentColor() == Color.WHITE) {
+                    drawPanel.setCurrentColor(Color.BLACK);
+                }
+
+            }
+        });
+
+        btnPencil = createIconButton(Color.WHITE, "Pencil.jpg", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawPanel.setCurrentShape("Pencil");
+                if (drawPanel.getCurrentColor() == Color.WHITE) {
+                    drawPanel.setCurrentColor(Color.BLACK);
+                }
+
+            }
+        });
+
+        btnEraser = createIconButton(Color.WHITE, "Eraser.png", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawPanel.setCurrentShape("Eraser");
+                drawPanel.setCurrentColor(Color.WHITE);
+
+            }
+        });
 
         btnClear = createIconButton(Color.WHITE, "Clear.png", new ActionListener() {
             @Override
@@ -111,7 +155,29 @@ class ButtonPanel extends JPanel {
             }
         });
 
+        btnUndo = createIconButton(Color.WHITE, "Undo.png", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnSave = createIconButton(Color.WHITE, "Save.png", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnOpen = createIconButton(Color.WHITE, "Open.png", new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
         this.setFocusable(true);
+
         add(checkFilled);
         add(btnRed);
         add(btnGreen);
@@ -122,43 +188,44 @@ class ButtonPanel extends JPanel {
         add(btnPencil);
         add(btnEraser);
         add(btnClear);
+        add(btnUndo);
+        add(btnSave);
+        add(btnOpen);
     }
 
-    private JButton createIconButton(Color color, ActionListener listener) {
-        JButton button = new JButton();
-        button.setBackground(color);
-        button.addActionListener(listener);
-        button.setPreferredSize(new Dimension(50, 40));
-        return button;
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
     }
 
     private JButton createIconButton(Color color, String fileLocation, ActionListener listener) {
+        // Get the URL of the image file
         URL imageURL = getClass().getResource(fileLocation);
         ImageIcon icon = new ImageIcon(imageURL);
         JButton button = new JButton(icon);
+        // set the background Color
         button.setBackground(color);
         button.addActionListener(listener);
+        // Set a specific size for the button
         button.setPreferredSize(new Dimension(50, 40));
         Image scaledImage = icon.getImage().getScaledInstance(50, 40, Image.SCALE_SMOOTH);
         button.setIcon(new ImageIcon(scaledImage));
         return button;
     }
 
-    private JButton createShapeButton(final String shape) {
-        JButton button = new JButton(shape);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                drawPanel.setCurrentShape(shape);
-            }
-        });
+    private JButton createIconButton(Color color, ActionListener listener) {
+
+        JButton button = new JButton();
+        // set the background Color
+        button.setBackground(color);
+        button.addActionListener(listener);
+        // Set a specific size for the button
+        button.setPreferredSize(new Dimension(50, 40));
         return button;
     }
+
 }
-
-
-
-
 
 class DrawPanel extends JPanel {
 
@@ -168,63 +235,142 @@ class DrawPanel extends JPanel {
     private String currentShape;
 
     DrawPanel() {
-        this.setBackground(Color.WHITE);
-        this.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                startX = e.getX();
-                startY = e.getY();
-            }
+        MyMouseListner ML = new MyMouseListner();
+        this.setFocusable(true);
+        this.addMouseListener(ML);
+        this.addMouseMotionListener(ML);
+    }
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                endX = e.getX();
-                endY = e.getY();
-                Graphics2D g2d = (Graphics2D) getGraphics();
-                g2d.setColor(currentColor);
-                if (currentShape.equals("Rectangle")) {
+    public class MyMouseListner implements MouseListener, MouseMotionListener {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            startX = e.getX();
+            startY = e.getY();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            endX = e.getX();
+            endY = e.getY();
+            Graphics2D g2d = (Graphics2D) getGraphics();
+            g2d.setColor(currentColor);
+            switch (currentShape) {
+                case "Rectangle":
                     if (isFilled) {
                         g2d.fillRect(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
                     } else {
                         g2d.drawRect(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
                     }
-                } else if (currentShape.equals("Oval")) {
+                    break;
+                case "Oval":
                     if (isFilled) {
                         g2d.fillOval(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
                     } else {
                         g2d.drawOval(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
                     }
-                } else if (currentShape.equals("Line")) {
+                    break;
+                case "Line":
                     g2d.drawLine(startX, startY, endX, endY);
-                }
-                g2d.dispose();
+                    break;
+                default:
+                    break;
             }
-        });
-        this.addMouseMotionListener(new MouseMotionAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                if (currentShape.equals("Free Hand")) {
-                    Graphics g = getGraphics();
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            Graphics g = getGraphics();
+            switch (currentShape) {
+                case "Pencil": {
                     g.setColor(currentColor);
                     g.drawLine(startX, startY, e.getX(), e.getY());
                     startX = e.getX();
                     startY = e.getY();
+                    break;
                 }
+                case "Eraser": {
+                    g.setColor(currentColor);
+                    g.fillRect(startX, startY, 40, 40);
+                    startX = e.getX();
+                    startY = e.getY();
+                    break;
+                }
+//                case "Rectangle":{
+//                    endX = e.getX();
+//                    endY = e.getY();
+//                    if (isFilled) {
+//                        g.fillRect(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
+//                    } else {
+//                        g.drawRect(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
+//                    }
+//                    break;
+//                }
+//                case "Oval":
+//                {
+//                    endX = e.getX();
+//                    endY = e.getY();
+//                    if (isFilled) {
+//                        g.fillOval(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
+//                    } else {
+//                        g.drawOval(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
+//                    }
+//                    break;
+//                }
+//                case "Line":
+//                {
+//                    endX = e.getX();
+//                    endY = e.getY();
+//                    g.drawLine(startX, startY, endX, endY);
+//                    break;
+//                }
+                default:
+                    break;
             }
-        });
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+
+        }
+
     }
 
     @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public void paint(Graphics g) {
+        super.paint(g);
+
     }
 
     public void setCurrentColor(Color color) {
         currentColor = color;
     }
 
+    public Color getCurrentColor() {
+        return currentColor;
+    }
+
     public void setCurrentShape(String shape) {
         currentShape = shape;
+    }
+
+    public String getCurrentShape() {
+        return currentShape;
     }
 
     public void setFilled(boolean filled) {
